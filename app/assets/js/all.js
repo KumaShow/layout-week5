5d4e702d39d53b409630c5a8069d5fa4efe27ae5
KumaShow <kumadth@gmail.com>
2022-08-07 23:59:05 +0800

    增加按鈕互動

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,4 +3,20 @@ $(document).ready(() => {
     $('.sidebar__link').removeClass('is-active');
     $(this).addClass('is-active');
   })
+
+  $('#grade-icon').click(function (e) {
+    $('#grade-icon').toggleClass('no-fill');
+  })
+
+  $('#assignment-reply').click(function (e) {
+    $('#editor').removeClass('d-none');
+    $('#assignment-nav').addClass('d-none');
+    
+  })
+
+  $('#editor-cancel').click(function (e) {
+    $('#editor').addClass('d-none');
+    $('#assignment-nav').removeClass('d-none');
+  })
+
 });
