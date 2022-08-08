65d6cc852bfc7eafc572d477263b8dbd9976ce8b
KumaShow <kumadth@gmail.com>
2022-08-08 23:08:48 +0800

    Update 2022-08-08T15:08:38.048Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,27 +1,24 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   $('.sidebar__link').click(function (e) {
     $('.sidebar__link').removeClass('is-active');
     $(this).addClass('is-active');
-  })
-
+  });
   $('#grade-icon').click(function (e) {
     $('#grade-icon').toggleClass('no-fill');
-  })
-
+  });
   $('#assignment-reply').click(function (e) {
     $('#editor').removeClass('d-none');
     $('#assignment-nav').addClass('d-none');
-    
-  })
-
+  });
   $('#editor-cancel').click(function (e) {
     $('#editor').addClass('d-none');
     $('#assignment-nav').removeClass('d-none');
-  })
-
+  });
   $('.btn-more').click(function (e) {
     $('.btn-more').toggleClass('d-none');
     $('.view-modal__btn').toggleClass('d-none');
-  })
-
+  });
 });
+//# sourceMappingURL=all.js.map
