cba932fba2667e886287a51d96cf53af0209634f
KumaShow <kumadth@gmail.com>
2022-08-10 11:37:17 +0800

    Update all.js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,27 +1,28 @@
-$(document).ready(() => {
-  $('.sidebar__link').click(function (e) {
+$(function() {
+  // sidebar
+  $('.sidebar__link').on('click', ()=> {
     $('.sidebar__link').removeClass('is-active');
-    $(this).addClass('is-active');
+    // $(this).addClass('is-active');
+    // $(this).toggleClass('is-active');
   })
-
-  $('#grade-icon').click(function (e) {
+  // assignment 頁
+  $('#grade-icon').on('click', ()=> {
     $('#grade-icon').toggleClass('no-fill');
   })
-
-  $('#assignment-reply').click(function (e) {
-    $('#editor').removeClass('d-none');
-    $('#assignment-nav').addClass('d-none');
-    
-  })
-
-  $('#editor-cancel').click(function (e) {
-    $('#editor').addClass('d-none');
-    $('#assignment-nav').removeClass('d-none');
-  })
-
-  $('.btn-more').click(function (e) {
+  // admin 頁
+  $('.btn-more').on('click', ()=> {
     $('.btn-more').toggleClass('d-none');
     $('.view-modal__btn').toggleClass('d-none');
+  })
+
+  $('#assignment-reply').on('click', ()=> {
+    $('#editor').removeClass('d-none');
+    $('#assignment-nav').addClass('d-none');
+  })
+
+  $('#editor-cancel').on('click', ()=> {
+    $('#editor').addClass('d-none');
+    $('#assignment-nav').removeClass('d-none');
   })
 
 });
