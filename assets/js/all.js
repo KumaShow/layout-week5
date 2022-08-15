981c4f992dd98197900100dc92e3594559c17aac
KumaShow <kumadth@gmail.com>
2022-08-15 16:59:51 +0800

    Update 2022-08-15T08:59:43.486Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,27 +1,27 @@
 "use strict";
 
 $(function () {
-  // sidebar
-  $('.sidebar__link').on('click', function () {
-    $('.sidebar__link').removeClass('is-active'); // $(this).addClass('is-active');
-    // $(this).toggleClass('is-active');
-  }); // assignment 頁
-
+  // assignment 頁
   $('#grade-icon').on('click', function () {
     $('#grade-icon').toggleClass('no-fill');
+  });
+  $('#assignment-btn-reply').on('click', function () {
+    $('#ckeditor').removeClass('d-none');
+    $('#assignment-reply').addClass('d-none');
+    $('#editor-reply').removeClass('d-none');
+  });
+  $('#editor-cancel').on('click', function () {
+    $('#ckeditor').addClass('d-none');
+    $('#editor-reply').addClass('d-none');
+    $('#assignment-reply').removeClass('d-none');
   }); // admin 頁
 
   $('.btn-more').on('click', function () {
     $('.btn-more').toggleClass('d-none');
     $('.view-modal__btn').toggleClass('d-none');
   });
-  $('#assignment-reply').on('click', function () {
-    $('#editor').removeClass('d-none');
-    $('#assignment-nav').addClass('d-none');
-  });
-  $('#editor-cancel').on('click', function () {
-    $('#editor').addClass('d-none');
-    $('#assignment-nav').removeClass('d-none');
-  });
+});
+ClassicEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
